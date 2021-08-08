7fc6f967217e2a87ffd7c4cbdfe4d7528e593309
anirbmuk <anirbmuk@live.in>
2021-08-08 19:31:54 +0530

    feat: added lookup post route

--- src/routes/status.route.js
+++ src/routes/status.route.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const Lookup = require('./../models/lookup.model');
 const authguard = require('./../middlewares/guard.mw');
 
+router.post('', authguard, async (req, res) => {
+    const newLookup = new Lookup(req.body);
+    try {
+        await newLookup.save();
+        return res.status(201).send(newLookup);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
 router.get('', authguard, async (req, res) => {
     try {
         const statuses = await Lookup.find();
